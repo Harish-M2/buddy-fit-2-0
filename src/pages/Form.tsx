b33d94f264a910d5f9a5f29a3db75bff5be8acf7
133353 <Harishmistry@rccl.com>
2023-09-27 17:06:00 +0100

    updates 22/09/23

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,12 +1,15 @@
 import React, { useRef, useState } from "react";
-import { MultiSelect, MultiSelectChangeEvent } from 'primereact/multiselect';
+import { MultiSelect, MultiSelectChangeEvent, Toast } from 'primereact/multiselect';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import { Button } from 'primereact/button';
-import { Toast } from 'primereact/toast';
 import './Form.css';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';   // theme
-import 'primereact/resources/primereact.css';                       // core css
-import 'primeicons/primeicons.css';                                 // icons
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
+import { useDarkMode } from "./DarkModeContext";
+
+// Rest of the code...
 
 interface City {
     name: string;
@@ -26,14 +29,12 @@ const Form: React.FC = () => {
 
     const show = () => {
         const itemArray = selectedSports.map((city, i) => city.name + (i < selectedSports.length - 1 ? ', ' : ''));
-
         toast.current?.show({ severity: 'success', summary: 'Form Submitted', detail: itemArray.join(', ') });
     };
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
 
-        // Check if there are no selected sports, and if so, prevent form submission.
         if (selectedSports.length === 0) {
             toast.current?.show({
                 severity: 'error',
@@ -43,43 +44,54 @@ const Form: React.FC = () => {
             return;
         }
 
-        // If there are selected sports, proceed with form submission and show success message.
         show();
 
-        // Reset the form after submission.
-        e.currentTarget.reset();
+        const form = e.target as HTMLFormElement;
+        form.reset();
     };
 
     const handleMultiSelectChange = (e: MultiSelectChangeEvent) => {
         setSelectedSports(e.value);
     };
 
-    const isFormFieldInvalid = (name: string) => !selectedSports.length;
-
-    const getFormErrorMessage = (name: string) => {
-        return isFormFieldInvalid(name) ? <small className="p-error">Please select at least one sport.</small> : <small className="p-error">&nbsp;</small>;
-    };
+    const { darkMode } = useDarkMode();
 
     return (
-        <div className="card flex justify-content-center">
-            <form onSubmit={handleSubmit} className="flex flex-column align-items-center gap-2">
-                <Toast ref={toast} />
-                <MultiSelect
-                    id="item"
-                    name="item"
-                    options={cities}
-                    value={selectedSports}
-                    onChange={handleMultiSelectChange}
-                    optionLabel="name"
-                    placeholder="Select Sports"
-                    maxSelectedLabels={3}
-                    className="w-full md:w-40rem"
-                />
-                {getFormErrorMessage('item')}
-                <Button type="submit" label="Submit" className="mt-2" />
-            </form>
-        </div>
-    )
-}
+        <IonPage className={`${darkMode ? 'dark-mode' : ''}`}>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Home</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent scrollY fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Sports</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonForm>
+                    <div className="card flex justify-content-center">
+                        <form onSubmit={handleSubmit} className="flex flex-column align-items-center gap-2">
+                            <Toast ref={toast} />
+                            <MultiSelect
+                                id="item"
+                                name="item"
+                                options={cities}
+                                value={selectedSports}
+                                onChange={handleMultiSelectChange}
+                                optionLabel="name"
+                                placeholder="Select Sports"
+                                maxSelectedLabels={3}
+                                className="w-full md:w-40rem"
+                            />
+                            {selectedSports.length === 0 && <small className="p-error">Please select at least one sport.</small>}
+                            <Button type="submit" label="Submit" className="mt-2" />
+                        </form>
+                    </div>
+                </IonForm>
+            </IonContent>
+        </IonPage>
+    );
+};
 
 export default Form;
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -17,14 +17,14 @@ const Tab2: React.FC = () => {
             <IonContent scrollY fullscreen>
                 <IonHeader collapse="condense">
                     <IonToolbar>
-                        <IonTitle size="large">t</IonTitle>
+                        <IonTitle size="large">Sports</IonTitle>
                     </IonToolbar>
                 </IonHeader>
 
                 <IonCard>
                     <img alt="example1" src="https://ionicframework.com/docs/img/demos/card-media.png" />
                     <IonCardHeader>
-                        <IonCardTitle>Goals</IonCardTitle>
+                        <IonCardTitle>Football</IonCardTitle>
                         <IonCardSubtitle>Osterley</IonCardSubtitle>
                     </IonCardHeader>
                     <IonCardContent>Our purpose-built, all-weather football clubs feature the very latest ProTurf synthetic grass all with individual floodlit arenas.
@@ -33,7 +33,7 @@ const Tab2: React.FC = () => {
                 <IonCard>
                     <img alt="example1" src="https://ionicframework.com/docs/img/demos/card-media.png" />
                     <IonCardHeader>
-                        <IonCardTitle>Goals</IonCardTitle>
+                        <IonCardTitle>Tennis</IonCardTitle>
                         <IonCardSubtitle>Osterley</IonCardSubtitle>
                     </IonCardHeader>
                     <IonCardContent>Our purpose-built, all-weather football clubs feature the very latest ProTurf synthetic grass all with individual floodlit arenas.
@@ -42,34 +42,7 @@ const Tab2: React.FC = () => {
                 <IonCard>
                     <img alt="example1" src="https://ionicframework.com/docs/img/demos/card-media.png" />
                     <IonCardHeader>
-                        <IonCardTitle>Goals</IonCardTitle>
-                        <IonCardSubtitle>Osterley</IonCardSubtitle>
-                    </IonCardHeader>
-                    <IonCardContent>Our purpose-built, all-weather football clubs feature the very latest ProTurf synthetic grass all with individual floodlit arenas.
-                        After your game, relive that perfect 1-2 or nutmeg in our state-of-the-art sports lounges with BT Sport on the big screen and free Wi-Fi.</IonCardContent>
-                </IonCard>
-                <IonCard>
-                    <img alt="example1" src="https://ionicframework.com/docs/img/demos/card-media.png" />
-                    <IonCardHeader>
-                        <IonCardTitle>Goals</IonCardTitle>
-                        <IonCardSubtitle>Osterley</IonCardSubtitle>
-                    </IonCardHeader>
-                    <IonCardContent>Our purpose-built, all-weather football clubs feature the very latest ProTurf synthetic grass all with individual floodlit arenas.
-                        After your game, relive that perfect 1-2 or nutmeg in our state-of-the-art sports lounges with BT Sport on the big screen and free Wi-Fi.</IonCardContent>
-                </IonCard>
-                <IonCard>
-                    <img alt="example1" src="https://ionicframework.com/docs/img/demos/card-media.png" />
-                    <IonCardHeader>
-                        <IonCardTitle>Goals</IonCardTitle>
-                        <IonCardSubtitle>Osterley</IonCardSubtitle>
-                    </IonCardHeader>
-                    <IonCardContent>Our purpose-built, all-weather football clubs feature the very latest ProTurf synthetic grass all with individual floodlit arenas.
-                        After your game, relive that perfect 1-2 or nutmeg in our state-of-the-art sports lounges with BT Sport on the big screen and free Wi-Fi.</IonCardContent>
-                </IonCard>
-                <IonCard>
-                    <img alt="example1" src="https://ionicframework.com/docs/img/demos/card-media.png" />
-                    <IonCardHeader>
-                        <IonCardTitle>Goals</IonCardTitle>
+                        <IonCardTitle>Golf</IonCardTitle>
                         <IonCardSubtitle>Osterley</IonCardSubtitle>
                     </IonCardHeader>
                     <IonCardContent>Our purpose-built, all-weather football clubs feature the very latest ProTurf synthetic grass all with individual floodlit arenas.
